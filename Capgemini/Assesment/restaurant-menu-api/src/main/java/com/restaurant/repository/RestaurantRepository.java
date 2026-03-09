ebb24f0fcbb5667b033361ae446be33281121338
Rahul Kumar Gupta <119124035+Rahul-Gupta55@users.noreply.github.com>
2026-03-09 09:39:54 +0530

    Delete Capgemini/Assesment/restaurant-menu-api directory

--- Capgemini/Assesment/restaurant-menu-api/src/main/java/com/restaurant/repository/RestaurantRepository.java
+++
@@ -1,8 +0,0 @@
-package com.restaurant.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.restaurant.entity.Restaurant;
-
-public interface RestaurantRepository extends JpaRepository<Restaurant,Long>{
-
-}
--- Capgemini/Assesment/restaurant-menu-api/src/main/java/com/restaurant/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.restaurant.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.restaurant.entity.User;
-
-public interface UserRepository extends JpaRepository<User,Long>{
-
-}
